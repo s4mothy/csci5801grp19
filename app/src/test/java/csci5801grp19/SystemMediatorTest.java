6202eba6e2dc376e4f5317d229cd905b335b91a5
s4mothy <s4mothy.dev@gmail.com>
2022-11-28 18:11:55 -0600

    fix error

--- app/src/test/java/csci5801grp19/SystemMediatorTest.java
+++ app/src/test/java/csci5801grp19/SystemMediatorTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SystemMediatorTest {
-  @test void SystemMediatorHasConstructor() {
+  @Test void SystemMediatorHasConstructor() {
     SystemMediator sm = new SystemMediator();
     assertTrue(sm instanceof SystemMediator);
   }
 
-  
+
 }

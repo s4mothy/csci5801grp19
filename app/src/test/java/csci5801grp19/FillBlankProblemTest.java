5388dc1204983d211f81b7382802a1eddb03b6dd
Daniel Swarts <swart179@umn.edu>
2022-12-13 22:59:24 -0600

    finished fill in the blank implementation

--- app/src/test/java/csci5801grp19/FillBlankProblemTest.java
+++ app/src/test/java/csci5801grp19/FillBlankProblemTest.java
@@ -3,79 +3,108 @@ package csci5801grp19;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FillBlankProblemTest {
-    @Test void  constructorTest() {
+    @Test
+    void constructorTest() {
         FillBlankProblem p = new FillBlankProblem("python");
-        
-        assertEquals("python",p.langEx);
+
+        assertEquals("python", p.langEx);
     }
 
-
-    @Test void  attemptOpenFile(){
+    @Test
+    void attemptOpenFile() {
         FillBlankProblem p = new FillBlankProblem("python");
-        int result = p.makeProblem("not a real path",0,0);
-        assertEquals(1,result);
+        int result = p.makeProblem("not a real path", 0, 0);
+        assertEquals(1, result);
     }
 
-    @Test void  OpenFile() {
+    @Test
+    void OpenFile() {
         FillBlankProblem p = new FillBlankProblem("python");
-        int result = p.makeProblem("src\\main\\resources\\source_code\\test_source_code\\primeNumber.py",0,0);
-        assertEquals(0,result);
+        int result = p.makeProblem("src\\main\\resources\\source_code\\test_source_code\\primeNumber.py", 0, 0);
+        assertEquals(0, result);
     }
 
-    @Test void  iscomment() {
+    @Test
+    void iscomment() {
         FillBlankProblem p = new FillBlankProblem("python");
         String keyword = "#comment";
         int result = p.categorizeToken(keyword);
-        assertEquals(0,result);
+        assertEquals(0, result);
     }
 
-    @Test void  isKeyword() {
+    @Test
+    void isKeyword() {
         FillBlankProblem p = new FillBlankProblem("python");
         String keyword = "if";
         int result = p.categorizeToken(keyword);
-        assertEquals(1,result);
+        assertEquals(1, result);
     }
 
-    @Test void  isArithmentic() {
+    @Test
+    void isArithmentic() {
         FillBlankProblem p = new FillBlankProblem("python");
         String keyword = "+";
         int result = p.categorizeToken(keyword);
-        assertEquals(2,result);
+        assertEquals(2, result);
     }
 
-    @Test void  isAssignment() {
+    @Test
+    void isAssignment() {
         FillBlankProblem p = new FillBlankProblem("python");
         String keyword = "=";
         int result = p.categorizeToken(keyword);
-        assertEquals(3,result);
+        assertEquals(3, result);
     }
 
-    @Test void  isComparison() {
+    @Test
+    void isComparison() {
         FillBlankProblem p = new FillBlankProblem("python");
         String keyword = "==";
         int result = p.categorizeToken(keyword);
-        assertEquals(4,result);
+        assertEquals(4, result);
     }
 
-    @Test void  isBitwise() {
+    @Test
+    void isBitwise() {
         FillBlankProblem p = new FillBlankProblem("python");
         String keyword = "|";
         int result = p.categorizeToken(keyword);
-        assertEquals(5,result);
+        assertEquals(5, result);
     }
 
-    @Test void  isOther() {
+    @Test
+    void isOther() {
         FillBlankProblem p = new FillBlankProblem("python");
         String keyword = "variableName";
         int result = p.categorizeToken(keyword);
-        assertEquals(6,result);
+        assertEquals(6, result);
     }
 
-    @Test void  toStringTest() {
+    @Test
+    void getFileSpecsTest() {
         FillBlankProblem p = new FillBlankProblem("python");
-        p.makeProblem("src\\main\\resources\\source_code\\test_source_code\\primeNumber.py",0,0);
+        File myFile;
+        String filePath = "src\\main\\resources\\source_code\\test_source_code\\getFileSpecsTest.py";
+
+        myFile = new File(filePath);
+            
+
+        int[] expected = {8, 1, 3, 2, 0, 94};
+        int[] actual = p.getFileSpecs(myFile);
+
+        assertEquals(expected[0], actual[0]);
+    }
+
+    @Test
+    void toStringTest() {
+        FillBlankProblem p = new FillBlankProblem("python");
+        p.makeProblem("src\\main\\resources\\source_code\\test_source_code\\primeNumber.py", 0, 0);
         int result = p.writeOutput("src\\main\\resources\\source_code\\test_source_code\\output.py");
-        assertEquals(0,result);
+        assertEquals(0, result);
     }
 }

de45ab92bea640cdc56e986bd5675535b7a97956
Daniel Swarts <swart179@umn.edu>
2022-12-14 22:34:07 -0600

    added random type selection to default constructor fill in the blank problem

--- app/src/main/java/csci5801grp19/FillBlankProblem.java
+++ app/src/main/java/csci5801grp19/FillBlankProblem.java
@@ -20,53 +20,55 @@ public class FillBlankProblem extends AbstractProblem {
 
     public String[] python_comparison = { "==", "!=", ">", "<", ">=", "<=" };
 
-    public String[] python_bitwise = {"&","|","^","~","<<",">>"};
+    public String[] python_bitwise = { "&", "|", "^", "~", "<<", ">>" };
 
     // TODO: add keyword arrays for other languages.
 
-    
     /**
-     * @brief Constructor 
+     * @brief Constructor
      * @param myFile is the opened source code file
-     * @param type is the type of keyword to replace
-     * @param debug is wheather to run in debug mode
+     * @param type   is the type of keyword to replace
+     * @param debug  is wheather to run in debug mode
      * 
      * @return instance of this class
      */
-    public FillBlankProblem(File myFile, int type, boolean debug){
+    public FillBlankProblem(File myFile, int type, boolean debug) {
 
         // initialize parameters
         this.question = "";
         this.question = "";
         this.solution = "";
         this.langEx = "." + myFile.getName().split("\\.")[1];
-        this.makeProblem(myFile,type,debug);
+        this.makeProblem(myFile, type, debug);
     }
 
     /**
-     * @brief default Constructor (doesn't specify parameters; type = 0, debug = false)
+     * @brief default Constructor (doesn't specify parameters; type = 0, debug =
+     *        false)
      * @param myFile is the opened source code file
      * 
      * @return instance of this class
      */
-    public FillBlankProblem(File myFile){
+    public FillBlankProblem(File myFile) {
 
         // initialize parameters
         this.question = "";
         this.question = "";
         this.solution = "";
         this.langEx = "." + myFile.getName().split("\\.")[1];
-        this.makeProblem(myFile,0,false);
+        Random rand = new Random();
+        int type = rand.nextInt(6);
+        this.makeProblem(myFile, type, false);
     }
 
     /**
-     * @brief null Constructor (doesn't make problem on its own, used 
-     * for debugging)
+     * @brief null Constructor (doesn't make problem on its own, used
+     *        for debugging)
      * @param lang is the language of the problem
      * 
      * @return instance of this class
      */
-    public FillBlankProblem(String lang){
+    public FillBlankProblem(String lang) {
 
         // initialize parameters
         this.question = "";
@@ -76,13 +78,16 @@ public class FillBlankProblem extends AbstractProblem {
     }
 
     /**
-     * @brief This is the main workhorse of this class. Populates the class variables
-     * with a proper, finished problem bassed on the specifications.
+     * @brief This is the main workhorse of this class. Populates the class
+     *        variables
+     *        with a proper, finished problem bassed on the specifications.
      * @param myFile the opened source code file.
-     * @param type determines which type of keyword or operator to replace with a blank. 
-     * 0 for keyword, 1 for arithmetic operator, 2 for assignment operator, 3 for 
-     * comparison operator, 4 for bitwise operator.
-     * @param debug either true for debug mode or false for noraml mode.
+     * @param type   determines which type of keyword or operator to replace with a
+     *               blank.
+     *               0 for keyword, 1 for arithmetic operator, 2 for assignment
+     *               operator, 3 for
+     *               comparison operator, 4 for bitwise operator.
+     * @param debug  either true for debug mode or false for noraml mode.
      * 
      * @return error code (0 if no issues)
      */
@@ -101,7 +106,7 @@ public class FillBlankProblem extends AbstractProblem {
             return 1;
         }
 
-        // this method works by randomly turning the n'th keyword into 
+        // this method works by randomly turning the n'th keyword into
         // a blank. We need the total number of keywords in the source code to
         // determine the range of possible random numbers.
 
@@ -111,12 +116,17 @@ public class FillBlankProblem extends AbstractProblem {
         // generate random value from 0 to number of keywords.
         Random rand = new Random();
         int upperbound = specs[type];
-        int n = rand.nextInt(upperbound); 
 
+        while (upperbound == 0) {
+            type = rand.nextInt(6);
+            upperbound = specs[type];
+        }
 
-        // if running in debugg mode, will print separators and parameter values 
-        // on the final product. 
-        if(debug){
+        int n = rand.nextInt(upperbound);
+
+        // if running in debugg mode, will print separators and parameter values
+        // on the final product.
+        if (debug) {
             this.question = "question:";
             this.question = "question:" + type;
             this.solution = "solution:" + n + "\n";
@@ -128,7 +138,7 @@ public class FillBlankProblem extends AbstractProblem {
         int i = 0;
         String[] line;
 
-        // iterate type to bring it in line with output from "categorizeToken" 
+        // iterate type to bring it in line with output from "categorizeToken"
         // function.
         type++;
 
@@ -137,31 +147,30 @@ public class FillBlankProblem extends AbstractProblem {
             line = fileReader.nextLine().split(" ");
 
             // ignore lines that begin with a comment, or are whitespace
-            if(line[0] != "#" && (line.length > 1)){
-                
+            if (line[0] != "#" && (line.length > 1)) {
+
                 // iterate through each line
                 for (String word : line) {
 
                     // this method reads which kind of keyword the token is
                     int category = categorizeToken(word);
-                    
 
-                    // when finding the beginning of a comment, 
+                    // when finding the beginning of a comment,
                     // ignore the rest of that line.
                     if (category == 0) {
                         break;
                     }
 
                     // if word is the correct type:
-                    else if (category == type){
+                    else if (category == type) {
 
                         // if the word is the n'th instance
-                        if (i == n){
+                        if (i == n) {
                             this.question += "_____";
                             this.solution += word;
                         }
 
-                        // if word isn't the blank, simply write it to the 
+                        // if word isn't the blank, simply write it to the
                         // output as is.
                         else {
                             this.question += word;
@@ -169,8 +178,8 @@ public class FillBlankProblem extends AbstractProblem {
                         i++;
                     }
 
-                    // if word isn't of interest, simply write it to the 
-                    // output as is. 
+                    // if word isn't of interest, simply write it to the
+                    // output as is.
                     else {
                         this.question += word;
                     }
@@ -190,29 +199,29 @@ public class FillBlankProblem extends AbstractProblem {
 
     /**
      * @brief Check to see if token contains a python keyword.
-     * @param word is the word we matching to python keywords. 
+     * @param word is the word we matching to python keywords.
      * 
      * @return 0 for comment, 1 for keyword, 2 for arithmetic operation, 3 for
-     * assignment operator, 4 for comparison operator, 5 for bitwise
-     * operator, 6 for other
+     *         assignment operator, 4 for comparison operator, 5 for bitwise
+     *         operator, 6 for other
      */
     public int categorizeToken(String word) {
 
-        if(word.contains("#")){
+        if (word.contains("#")) {
             return 0;
         }
 
         // compile a list of every category of keyword in python
-        String[][] python_categories = {this.python_keywords,
-            this.python_arithmetic,this.python_assignment,
-            this.python_comparison,this.python_bitwise};
+        String[][] python_categories = { this.python_keywords,
+                this.python_arithmetic, this.python_assignment,
+                this.python_comparison, this.python_bitwise };
 
         int i = 1;
 
         // iterate over the words and return number for category
-        for (String[] category : python_categories){
-            for (String keyword : category){
-                if(word.equals(keyword)){
+        for (String[] category : python_categories) {
+            for (String keyword : category) {
+                if (word.equals(keyword)) {
                     return i;
                 }
             }
@@ -227,13 +236,13 @@ public class FillBlankProblem extends AbstractProblem {
      * @brief gets number of keywords in file
      * @param myFile is the open file we are analysing
      * 
-     * @return list of counts. the index of the array signifies 
-     * the type of keyword counted there. For example, result[0] is 
-     * number of keywords, result[1] is the number of arithmetic operators,
-     * etc.
+     * @return list of counts. the index of the array signifies
+     *         the type of keyword counted there. For example, result[0] is
+     *         number of keywords, result[1] is the number of arithmetic operators,
+     *         etc.
      */
     public int[] getFileSpecs(File myFile) {
-        int[] result = {0,0,0,0,0,0};
+        int[] result = { 0, 0, 0, 0, 0, 0 };
         String[] line;
         Scanner fileReader;
 
@@ -254,8 +263,8 @@ public class FillBlankProblem extends AbstractProblem {
             line = fileReader.nextLine().split(" ");
 
             // ignore lines that begin with a comment, or are whitespace
-            if(line[0] != "#" && (line.length > 2)){
-                
+            if (line[0] != "#" && (line.length > 2)) {
+
                 for (String word : line) {
 
                     // read categorizeToken for more info
@@ -265,7 +274,7 @@ public class FillBlankProblem extends AbstractProblem {
                     if (category == 0) {
                         break;
                     }
-    
+
                     // add 1 to proper category
                     else {
                         category--;
@@ -284,7 +293,7 @@ public class FillBlankProblem extends AbstractProblem {
      * @brief string form of question.
      * 
      * @return Returns String of problem, which consist of three blocks of text;
-     * question on top, options in middle, solution bottom.
+     *         question on top, options in middle, solution bottom.
      */
     public String toString() {
         String str = question;
@@ -296,7 +305,7 @@ public class FillBlankProblem extends AbstractProblem {
 
     /**
      * @brief for debugging purposes. Writes the toString version of working class
-     * to a specified file path.
+     *        to a specified file path.
      * @param filepath is the location of the output.
      * 
      * @return error code. 0 if no errors.
